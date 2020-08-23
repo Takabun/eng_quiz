cd5d08bfbf9bed6cc2ae303d2852f0a838892701
Takabun <takafumi.kido@gmail.com>
2020-08-23 16:27:42 +0900

    APIとDBをそれぞれDocker化 & Docker-Composeで連携出来た！

--- api/controller/mysql.go
+++ api/controller/mysql.go
@@ -6,7 +6,10 @@ import (
 )
 
 func OpenSQLiteConnection() *gorm.DB {
-    db, err := gorm.Open("mysql",  "root:@/go_trial?parseTime=true&loc=Asia%2FTokyo")
+    // db, err := gorm.Open("mysql",  "root:@/go_trial?parseTime=true&loc=Asia%2FTokyo")
+    // db, err := gorm.Open("mysql",  "root:@tcp(db:3306)/go_trial?parseTime=true&loc=Asia%2FTokyo")
+    db, err := gorm.Open("mysql",  "root:password@tcp(db:3306)/go_trial?parseTime=true&loc=Asia%2FTokyo")
+    
     if err != nil {
         panic("failed to connect database.")
     }
