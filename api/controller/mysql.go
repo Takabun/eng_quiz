814bdcbc7a18d9b21a2bfa050fe2224a8247b43b
Takabun <takafumi.kido@gmail.com>
2021-01-31 19:36:42 +0900

    k8sのIP問題を完全解決！

--- api/controller/mysql.go
+++ api/controller/mysql.go
@@ -1,13 +1,20 @@
 package controller
 
 import (
+    "os"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func OpenSQLiteConnection() *gorm.DB {
     // db, err := gorm.Open("mysql",  "root:@/eng_quiz?parseTime=true&loc=Asia%2FTokyo")
-    db, err := gorm.Open("mysql", "root:password@tcp(db:3306)/eng_quiz?parseTime=true&loc=Asia%2FTokyo")
+    // db, err := gorm.Open("mysql", "root:password@tcp(db:3306)/eng_quiz?parseTime=true&loc=Asia%2FTokyo")
+    dbUrl := os.Getenv("DB_URL")
+    if dbUrl == "" {
+      dbUrl = "root:password@tcp(db:3306)/eng_quiz?parseTime=true&loc=Asia%2FTokyo"
+	}
+    
+    db, err := gorm.Open("mysql", dbUrl)
     if err != nil {
         panic("failed to connect database.")
     }
---
+++ db/init_table_command.sql
@@ -0,0 +1,92 @@
+CREATE TABLE `tags` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `name` varchar(255) DEFAULT NULL, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `idx_tags_deleted_at` (`deleted_at`) \
+) CHARSET=utf8;
+
+
+CREATE TABLE `questions` (   \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `text` varchar(1000) DEFAULT NULL, \
+  `user` varchar(255) NOT NULL DEFAULT '', \
+  `default_image` int(11) DEFAULT NULL, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `idx_questions_deleted_at` (`deleted_at`) \
+) CHARSET=utf8;
+
+
+CREATE TABLE `answers` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `question_id` int(11) unsigned NOT NULL, \
+  `text` varchar(1000) DEFAULT NULL, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  UNIQUE KEY `question_id` (`question_id`), \
+  KEY `idx_answers_deleted_at` (`deleted_at`), \
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) \
+) CHARSET=utf8;
+
+
+CREATE TABLE `question_images` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `question_id` int(11) unsigned DEFAULT NULL, \
+  `name` varchar(255) DEFAULT NULL, \
+  `url` text, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `question_id` (`question_id`), \
+  KEY `idx_question_images_deleted_at` (`deleted_at`), \
+  CONSTRAINT `question_images_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) \
+) CHARSET=utf8;
+
+
+CREATE TABLE `answer_images` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `answer_id` int(11) unsigned DEFAULT NULL, \
+  `name` varchar(255) DEFAULT NULL, \
+  `url` text, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `answer_id` (`answer_id`), \
+  KEY `idx_answer_images_deleted_at` (`deleted_at`), \
+  CONSTRAINT `answer_images_ibfk_1` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`) \
+) CHARSET=utf8;
+
+
+CREATE TABLE `comments` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `question_id` int(11) unsigned NOT NULL, \
+  `user` varchar(255) NOT NULL DEFAULT '', \
+  `text` varchar(1000) DEFAULT NULL, \
+  `created_at` datetime DEFAULT NULL, \
+  `updated_at` datetime DEFAULT NULL, \
+  `deleted_at` datetime DEFAULT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `question_id` (`question_id`), \
+  KEY `idx_comments_deleted_at` (`deleted_at`), \
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) \
+) CHARSET=utf8;
+
+CREATE TABLE `question_tags` ( \
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
+  `question_id` int(11) unsigned NOT NULL, \
+  `tag_id` int(11) unsigned NOT NULL, \
+  PRIMARY KEY (`id`), \
+  KEY `question_id` (`question_id`), \
+  KEY `tag_id` (`tag_id`), \
+  CONSTRAINT `question_tags_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`), \
+  CONSTRAINT `question_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) \
+) CHARSET=utf8;
